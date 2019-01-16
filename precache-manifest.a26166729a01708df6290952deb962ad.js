ce85d279218c29e3020e3e5fb9e3f9d4c6e2e6bf
Ruosi Wang <ruos925@gmail.com>
2019-01-15 23:21:40 -0500

    Updates

---
+++ precache-manifest.a26166729a01708df6290952deb962ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2acd8e27bb00f0a30539",
+    "url": "/static/js/main.2acd8e27.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "2acd8e27bb00f0a30539",
+    "url": "/static/css/main.3a8dd312.chunk.css"
+  },
+  {
+    "revision": "8786b92e13ea824c46bb75b4943db1de",
+    "url": "/index.html"
+  }
+];
