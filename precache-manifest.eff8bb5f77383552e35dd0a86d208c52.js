eb80122cca6fd61e02960536aa25dc7dc0aef461
Ruosi Wang <ruos925@gmail.com>
2019-01-15 23:37:02 -0500

    Updates

---
+++ precache-manifest.eff8bb5f77383552e35dd0a86d208c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "7ad88e4e03ff2ef7354b",
+    "url": "/react-gh-pages/static/js/main.7ad88e4e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7ad88e4e03ff2ef7354b",
+    "url": "/react-gh-pages/static/css/main.3a8dd312.chunk.css"
+  },
+  {
+    "revision": "f76fdac2669f249f514e53de4722e6ae",
+    "url": "/react-gh-pages/index.html"
+  }
+];
